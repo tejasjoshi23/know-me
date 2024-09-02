663e0f5580751868c3830ac6a11e8167171368b0
Tejas Joshi <95303369+tejasjoshi23@users.noreply.github.com>
2024-09-03 04:43:36 +0530

    err:resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { About, Contact, Hero, Navbar } from "./components";
-import { SkyAndBirds } from "./components/canvas";
 import "./App.css";
 
 function App() {
