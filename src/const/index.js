acce163a21b9bfffc490c15bcf27b40e48f642b2
Tejas Joshi <95303369+tejasjoshi23@users.noreply.github.com>
2024-09-03 04:36:03 +0530

    app:build

--- src/const/index.js
+++ src/const/index.js
@@ -1,19 +1,15 @@
 export const ABOUT_TEXTS = {
-  about1: "Tejas is Best !!",
-  about2: "Tejas is best with a bit more information.",
-  about3: "Tejas is best with even more information than before.",
-  about4: "Tejas is best with quite a bit more information, showcasing more details.",
-  about5: "Tejas is best with a substantial amount of information to provide a clearer picture. Tejas is best with a substantial amount of information to provide a clearer picture. Tejas is best with a substantial amount of information to provide a clearer picture. Tejas is best with a substantial amount of information to provide a clearer picture. Tejas is best with a substantial amount of information to provide a clearer picture. Tejas is best with a substantial amount of information to provide a clearer picture.",
-  about6: `Welcome to my realm! This is Tejas Joshi, a final-year Computer Science Engineering enthusiast at IIIT Vadodara. 🎓✨
-Dance with me in the code symphony where languages like C, C++, and Python weave tales, and the art of web development springs to life through HTML, CSS, and JavaScript. 🌐💻
+  about1: `Tejas is Best !!`,
 
-As a creator and explorer, I craft dynamic web experiences using React.js and Node.js, orchestrating seamless web development and immersive 3D game environments. The digital canvas is my playground, where Unity3D comes alive with AR applications. 🕹️🚀
+  about2: `As a Computer Science Engineering student at IIIT Vadodara, I’m proficient in languages like C, C++, and Python. I also work with HTML, CSS, and JavaScript, to build engaging and interactive web applications.`,
 
-Beyond the screen, my journey extends to the realms of leadership and creativity. I've orchestrated TEDxIIITV, navigated literature fests, and steered the ship as the Chief Editor of the annual institute magazine 'Nakshatra.' 📚✨
+  about3: `I’ completed BTech CSE at IIIT Vadodara tat 2024, specializing in web development with React.js and Node.js. I build responsive and dynamic websites and am well-versed in C, C++, and Python, which helps me tackle complex problems. My interests also extend to 3D development and augmented reality using Unity3D, adding a creative dimension to my technical skills.`,
 
-Join me as we venture into the future, where technology meets creativity, and every line of code tells a story. From NDA qualifications to TEDx triumphs, every step is a pixel in the grand mosaic of my digital odyssey. 🌟✨
+  about4: `I’m skilled in multiple programming languages, including C, C++, and Python. My expertise in web development shines through my work with React.js and Node.js, where I craft user-friendly applications. I also explore 3D development and AR with Unity3D to create immersive experiences. Beyond my technical pursuits, I’m actively involved in leadership roles, having organized events like TEDxIIITV and managed the institute magazine 'Nakshatra.'`,
 
-Welcome to the intersection of passion and proficiency, where the possibilities are as vast as the digital horizon. Let's code, create, and conquer together! 💻🚀✨`
+  about5: `To give you a clearer picture of who I am, here’s a more detailed overview. I’m Tejas Joshi, a graduated Computer Science Engineering student at IIIT Vadodara with a diverse skill set. I’m proficient in C, C++, and Python, which I use to tackle complex problems effectively. In web development, I leverage React.js and Node.js to build dynamic and responsive applications, integrating modern technologies like HTML, CSS, and JavaScript. My creative side comes to life in 3D game development and augmented reality through Unity3D, offering immersive and interactive experiences. Additionally, my leadership and organizational skills are demonstrated through my roles in TEDxIIITV, literature fests, and as Chief Editor of 'Nakshatra.' Each facet of my journey reflects a blend of technical expertise, creativity, and leadership.`,
+
+  about6: `Welcome to my realm! This is Tejas Joshi, a Computer Science Engineering enthusiast, completed journey at IIIT Vadodara this year. 🎓✨Dance with me in the code symphony where languages like C, C++, and Python weave tales, and the art of web development springs to life through HTML, CSS, and JavaScript. 🌐💻 As a creator and explorer, I craft dynamic web experiences using React.js and Node.js, orchestrating seamless web development and immersive 3D game environments. The digital canvas is my playground, where Unity3D comes alive with AR applications. 🕹️🚀 Beyond the screen, my journey extends to the realms of leadership and creativity. I've orchestrated TEDxIIITV, navigated literature fests, and steered the ship as the Chief Editor of the annual institute magazine 'Nakshatra.' 📚✨ Join me as we venture into the future, where technology meets creativity, and every line of code tells a story. From NDA qualifications to TEDx triumphs, every step is a pixel in the grand mosaic of my digital odyssey. 🌟✨ Welcome to the intersection of passion and proficiency, where the possibilities are as vast as the digital horizon. Let's code, create, and conquer together! 💻🚀✨`,
 };
 
 export const Emojis = ["😅", "😁", "😉", "😃", "😎", "🤩"];
